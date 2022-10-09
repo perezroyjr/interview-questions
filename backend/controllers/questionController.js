21b6ebe0f3c90ad998a705ccca0d15c61451117e
Roy Perez Jr <perezroyjr@yahoo.com>
2022-10-09 10:12:50 -0500

    Return random findone

--- backend/controllers/questionController.js
+++ backend/controllers/questionController.js
@@ -6,7 +6,18 @@ const Behavioral = require('../models/behavioralModel')
 //@route GET /api/programming-question
 //@access Public
 const getProgrammingQuestion = asyncHandler( async (request, response) => {
-	const questions = await Question.findOne() 
+	const questions = await Question.count().exec(function (err, count) {
+
+  // Get a random entry
+  var random = Math.floor(Math.random() * count)
+
+  // Again query all users but only fetch one offset by our random #
+  Question.findOne().skip(random).exec(
+    function (err, result) {
+      // Tada! random user
+      console.log(result) 
+    })
+})
 	response.json(questions)
 })
 
